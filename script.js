facff0d3fdc5c25e59b33dc91704d7cbb349a7dd
micaela1001 <micaela1001.sh@gmail.com>
2023-01-26 10:02:11 +0900

    プルリクエスト作成

---
+++ script.js
@@ -0,0 +1,12 @@
+for (let number = 1; number <100; number++) {
+  if (number % 5 === 0 && number % 3 === 0) {
+    console.log("Fizz" + "Buzz" + number);
+  } else if (number % 5 === 0) {
+    console.log("Buzz" + number);
+  } else if (number % 3 === 0) {
+    console.log("Fizz" + number);
+  } else {
+    console.log();
+  }
+}
+
