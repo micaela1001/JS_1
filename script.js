2cb2b37c51488c561a1b41f19a49539a2ebfed9d
micaela1001 <micaela1001.sh@gmail.com>
2023-01-27 14:33:45 +0900

    0127

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-for (let number = 1; number < 100; number++) {
-  if (number % 5 === 0 && number % 3 === 0) {
-    console.log("Fizz" + "Buzz" + number);
-  } else if (number % 5 === 0) {
-    console.log("Buzz" + number);
-  } else if (number % 3 === 0) {
-    console.log("Fizz" + number);
-  } else {
-    console.log();
+for (let i = 1; i < 100; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log(`FizzBuzz${i}`);
+  } else if (i % 5 === 0) {
+    console.log(`Buzz${i}`);
+  } else if (i % 3 === 0) {
+    console.log(`Fizz${i}`);
   }
 }
